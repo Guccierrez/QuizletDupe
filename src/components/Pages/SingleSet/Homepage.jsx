d5333a056873072b7b2bf2f90e94b92c71678e0e
Fabian Gutierrez <fabiangutierrez580@gmail.com>
2023-01-09 14:43:45 -0600

    got single card page to display but nothing rendered

---
+++ src/components/Pages/SingleSet/Homepage.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+
+export default function ({question, answer, id}) {
+  let [visibility, setVisbility] = useState("hidden")
+
+  function handleClick() {
+        setVisbility(visibility === 'hidden' ? 'visible' : 'hidden');
+      }
+
+  return (     
+    <div className="card" style={{ width:"30rem",  backgroundColor: "rgba(0, 0, 0, .075)",margin:"30px"}}>
+         
+          <h5 className="card-title">Question: {question}</h5>
+          <p className="card-text" style={{ visibility:visibility }} >Answer: {answer}</p>
+          <button onClick={handleClick}>show answer</button>
+          <Link to={`/singleSet/${id}`}>
+        hello
+      </Link>
+    </div> 
+  );
+}
+
+
+
---
+++ src/components/Pages/SingleSet/SingleCard.jsx
@@ -0,0 +1,17 @@
+import React  from 'react'
+import { useParams } from 'react-router-dom'
+import SingleSet from './SingleSet'
+
+
+const SingleCard = () => {
+    const params = useParams()
+
+  return (
+    <div>
+    
+  
+    </div>
+  )
+}
+
+export default SingleCard
